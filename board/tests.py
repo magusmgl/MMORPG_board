57312dde3972e3f4e7dcc6fad6f189eac0c92c4b
Maksim <magus.mgl@yandex.ru>
2022-10-18 02:34:16 +0500

    new test for board app

--- board/tests.py
+++ board/tests.py
@@ -1,12 +1,24 @@
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import reverse, resolve
+
+from .views import BoardPageView
 
 
 # Create your tests here.
 class BoardPageTest(TestCase):
+    def setUp(self) -> None:
+        url = reverse('notice_board')
+        self.response = self.client.get(url)
+
     def test_url_exists_at_correct_location(self):
-        responce = self.client.get('/')
-        self.assertEqual(responce.status_code, 200)
-    def test_boardpage_url_name(self):
-        responce = self.client.get(reverse('notice_board'))
-        self.assertEqual(responce.status_code, 200)
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_boardpage_template(self):
+        self.assertTemplateUsed(self.response, 'board.html')
+
+    # def test_boardpage_contains_correct_html(self):
+    #     self.assertContains(self.response, 'notice board')
+
+    def test_boardpage_url_resolves_boardpageview(self):
+        view = resolve('/')
+        self.assertEqual(view.func.__name__, BoardPageView.as_view().__name__)
