2d67265dce3eca01bfbd1610cc343e1df775fe14
Maksim <magus.mgl@yandex.ru>
2022-10-23 21:46:30 +0500

    add views and usrl for board app

--- board/urls.py
+++ board/urls.py
@@ -1,8 +1,20 @@
 from django.urls import path
 
-from .views import AdListView, AdDetailView
+from .views import (
+    AdsListView,
+    UserAdsListVies,
+    AdDetailView,
+    CreateAdView,
+    AdUpdateView,
+    AdDeleteView,
+)
 
 urlpatterns = [
-    path('', AdListView.as_view(), name='ad_list'),
+    path('', AdsListView.as_view(), name='ads_list'),
     path('<uuid:pk>/', AdDetailView.as_view(), name='ad_detail'),
+    path('create_ads/', CreateAdView.as_view(), name='create_ad'),
+    path('user_ads/', UserAdsListVies.as_view(), name='user_ads_list'),
+    path('user_ads/<uuid:pk>/edit/', AdUpdateView.as_view(), name='ad_edit'),
+    path('user_ads/<uuid:pk>/delete/', AdDeleteView.as_view(), name='ad_delete'),
+
 ]
--- board/views.py
+++ board/views.py
@@ -1,16 +1,67 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy, reverse
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    DeleteView,
+    UpdateView,
+)
+
 from .models import Advertisement
+from .forms import AdvertisementForm
 
 
 # Create your views here.
-class AdListView(ListView):
+class AdsListView(ListView):
     model = Advertisement
-    context_object_name = 'ad_list'
+    ordering = '-date'
+    context_object_name = 'ads_list'
     template_name = 'board/ad_list.html'
+    paginate_by = 10
+
+
+class UserAdsListVies(ListView):
+    model = Advertisement
+    ordering = '-date'
+    context_object_name = 'ads_list'
+    template_name = 'board/ad_list.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        user = self.request.user
+        return Advertisement.objects.filter(author=user)
 
 
 class AdDetailView(DetailView):
     model = Advertisement
     context_object_name = 'ad'
     template_name = 'board/ad_detail.html'
+
+
+class CreateAdView(CreateView):
+    form_class = AdvertisementForm
+    context_object_name = 'ad'
+    template_name = 'board/ad_create.html'
+
+    def form_valid(self, form):
+        form.instance.author=self.request.user
+        # ad = form.save(commit=False)
+        # ad.author = self.request.user
+        # ad.save()
+        return super().form_valid(form)
+
+class AdUpdateView(UpdateView):
+    model = Advertisement
+    form_class = AdvertisementForm
+    template_name = 'board/ad_create.html'
+    context_object_name = 'ad'
+
+
+class AdDeleteView(DeleteView):
+    model = Advertisement
+    template_name = 'board/ad_delete.html'
+    success_url = reverse_lazy('ads_list')
+    context_object_name = 'ad'
+
+
