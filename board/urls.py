cc495582af4c8f04456c81207288b0bc93dcd7bd
Maksim <magus.mgl@yandex.ru>
2022-10-25 10:20:41 +0500

    fix name views

--- board/urls.py
+++ board/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from .views import (
     AdsListView,
-    UserAdsListVies,
+    UserAdsListView,
     AdDetailView,
     CreateAdView,
     AdUpdateView,
@@ -13,7 +13,7 @@ urlpatterns = [
     path('', AdsListView.as_view(), name='ads_list'),
     path('<uuid:pk>/', AdDetailView.as_view(), name='ad_detail'),
     path('create_ads/', CreateAdView.as_view(), name='create_ad'),
-    path('user_ads/', UserAdsListVies.as_view(), name='user_ads_list'),
+    path('user_ads/', UserAdsListView.as_view(), name='user_ads_list'),
     path('user_ads/<uuid:pk>/edit/', AdUpdateView.as_view(), name='ad_edit'),
     path('user_ads/<uuid:pk>/delete/', AdDeleteView.as_view(), name='ad_delete'),
 
