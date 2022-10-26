cfecac868420410a99282a09ea9cc2ae291bb57c
Maksim <magus.mgl@yandex.ru>
2022-10-27 03:49:42 +0500

    fix reply filter

--- board/filters.py
+++ board/filters.py
@@ -1,17 +1,19 @@
 import django_filters
 
-from .models import Reply
+from .models import Reply, Advertisement
 from django.contrib.auth import get_user_model
 
+
 class ReplyFilter(django_filters.FilterSet):
-    user = get_user_model()
     advertise = django_filters.CharFilter(field_name='advertise__title',
                                           label='advertise title:',
                                           lookup_expr='icontains'
                                           )
+    user = get_user_model()
     author = django_filters.ModelChoiceFilter(queryset=user.objects.all(),
-                                            label = 'author of reply:',
-                                        )
+                                              label='author of reply:',
+                                              empty_label='author is not select'
+                                              )
 
     reply = django_filters.CharFilter(field_name='reply',
                                       label='reply text:',
