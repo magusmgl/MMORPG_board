9b2224ab44a012f60d19cdf154dc69b5aa03b02f
Maksim <magus.mgl@yandex.ru>
2022-10-27 03:51:13 +0500

    fix urls for board app

--- board/urls.py
+++ board/urls.py
@@ -7,16 +7,19 @@ from .views import (
     CreateAdView,
     AdUpdateView,
     AdDeleteView,
-    ReplaysAdsListView,
+    ReplaysSearchView,
+    ReplyDeleteView,
+    accept_reply,
 )
 
 urlpatterns = [
     path('', AdsListView.as_view(), name='ads_list'),
     path('<uuid:pk>/', AdDetailView.as_view(), name='ad_detail'),
     path('create_ads/', CreateAdView.as_view(), name='create_ad'),
-    path('replays/', ReplaysAdsListView.as_view(), name='replays_list'),
     path('user_ads/', UserAdsListView.as_view(), name='user_ads_list'),
     path('user_ads/<uuid:pk>/edit/', AdUpdateView.as_view(), name='ad_edit'),
     path('user_ads/<uuid:pk>/delete/', AdDeleteView.as_view(), name='ad_delete'),
-
+    path('replays/', ReplaysSearchView.as_view(), name='replays_list'),
+    path('replays/<int:pk>/delete/', ReplyDeleteView.as_view(), name='reply_delete'),
+    path('replays/<int:pk>/accept/', accept_reply, name='reply_accept'),
 ]
