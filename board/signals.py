3004bb23e4f03b32e31a59da50b65f643807c8e9
Maksim <magus.mgl@yandex.ru>
2022-10-28 01:11:13 +0500

    add signal and task for email notification

--- board/signals.py
+++ board/signals.py
@@ -2,7 +2,10 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from .models import Reply
-from .task import notification_about_receiving_new_reply
+from .task import (
+    notification_about_receiving_new_reply,
+    notification_about_accept_reply
+)
 
 
 @receiver(post_save, sender=Reply)
@@ -11,4 +14,4 @@ def notify_users_for_email(sender, instance, created, **kwargs):
         notification_about_receiving_new_reply.delay(reply_pk=instance.pk)
     else:
         if instance.is_accept:
-            pass
+            notification_about_accept_reply.delay(reply_pk=instance.pk)
--- board/task.py
+++ board/task.py
@@ -11,20 +11,37 @@ def notification_about_receiving_new_reply(reply_pk):
     reply = Reply.objects.get(pk=reply_pk)
     username = reply.advertise.author.get_username()
     author_email = reply.advertise.author.email
-    email_html = create_email_template(username, reply)
+    email_html = create_email_template(username,
+                                       reply,
+                                       template='board/email_for_adding_new_reply.html')
     send_email(html_content=email_html,
                letter_subject='New response to your ad',
                email_to=author_email)
 
-def create_email_template(user, reply):
+
+@shared_task
+def notification_about_accept_reply(reply_pk):
+    reply = Reply.objects.get(pk=reply_pk)
+    username = reply.author.get_username()
+    author_email = reply.author.email
+    email_html = create_email_template(username,
+                                       reply,
+                                       template='board/email_for_accept_reply.html')
+    send_email(html_content=email_html,
+               letter_subject='Reply has been accepted',
+               email_to=author_email)
+
+
+def create_email_template(user, reply, template):
     html = render_to_string(
-        template_name='board/email_for_adding_new_reply.html',
+        template_name=template,
         context={
             'user': user,
             'reply': reply
         }
     )
     return html
+
 
 def send_email(html_content, letter_subject, email_to):
     from_email = settings.DEFAULT_FROM_EMAIL
