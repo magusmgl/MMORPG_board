59d2c6cb0345bd93fab0ca34fbb4606a5c5f3321
Maksim <magus.mgl@yandex.ru>
2022-10-27 22:47:24 +0500

    add signals to adding new reply

--- board/signals.py
+++ board/signals.py
@@ -1,13 +1,14 @@
 from django.db.models.signals import post_save
-from django.core.mail import mail_managers
 from django.dispatch import receiver
 
 from .models import Reply
+from .task import notification_about_receiving_new_reply
 
 
 @receiver(post_save, sender=Reply)
-def notify_author_for_new_reply(sender, instance, created, **kwargs):
-    mail_managers(
-        subject=f'{instance.author} {instance.advertise}',
-        message=instance.advertise
-    )
+def notify_users_for_email(sender, instance, created, **kwargs):
+    if created:
+        notification_about_receiving_new_reply.delay(reply_pk=instance.pk)
+    else:
+        if instance.is_accept:
+            pass
