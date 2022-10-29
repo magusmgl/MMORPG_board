2246ca1e197151f0e61e273444b353f45890c19e
Maksim <magus.mgl@yandex.ru>
2022-10-29 13:24:36 +0500

    fix project urls

--- django_project/urls.py
+++ django_project/urls.py
@@ -4,13 +4,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-          # Django admin
-          path('admin/', admin.site.urls),
-              # Managment user
-          # path('accounts/', include('django.contrib.auth.urls')),
-          # ckeditor
-          path('ckeditor/', include('ckeditor_uploader.urls')),
-          # Local apps
-          path('accounts/', include('allauth.urls')),
-          path('', include('board.urls')),
-          ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  # Django admin
+                  path('admin/', admin.site.urls),
+                  # ckeditor
+                  path('ckeditor/', include('ckeditor_uploader.urls')),
+                  # Managment user
+                  path('accounts/', include('accounts.urls')),
+                  # Local apps
+                  path('accounts/', include('allauth.urls')),
+                  path('', include('board.urls')),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
