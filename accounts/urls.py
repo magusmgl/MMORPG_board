651fae78a702acbef3564543f720f2a51d666bae
Maksim <magus.mgl@yandex.ru>
2022-10-29 13:24:06 +0500

    add urls for accounts app

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,17 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .views import SignupPageView
+from .views import (
+    SignupPageView,
+    usual_login_view,
+    send_onetime_code,
+    login_with_code_view
+)
+
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('signup/', SignupPageView.as_view(), name='signup')
+    path('login/entry_code/', login_with_code_view, name='code_entry_page'),
+    path('login/send_code', send_onetime_code, name='send_onetime_code'),
+    path('login/', usual_login_view, name='login_with_verification_code'),
 ]
